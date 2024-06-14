4959d008721632d172fd64412be900c8d655da8d
yangtingtingsir <147301047+yangtingtingsir@users.noreply.github.com>
2024-06-14 08:57:56 +0800

    addfootgame

---
+++ js/CLang.min.js
@@ -0,0 +1,48 @@
+TEXT_TEAM = ["Algeria", "Argentina", "Australia", "Brasil", "Cameroon", "Cile",
+    "South Corea", "Ivory Coast", "Croatia", "Denmark", "France", "Germany",
+    "Ghana", "Japan", "Greece", "Honduras", "England", "Italy", "Mexico", "Nigeria",
+    "New Zealand", "Netherlands", "Paraguay", "Portugal", "Russia", "Serbia", "Slovenia",
+    "Spain", "South Africa", "Switzerland", "Uruguay", "USA"];
+
+TEXT_TEAM_CODE = ["ALG", "ARG", "AUS", "BRA", "CMR", "CHI", "KOR", "CIV", "CRO", "DEN", "FRA",
+    "GER", "GHA", "JPN", "GRE", "HON", "ENG", "ITA", "MEX", "NIG", "NZL", "NED", "PAR", "POR",
+    "RUS", "SRB", "SVN", "ESP", "RSA", "SUI", "URU", "USA"];
+
+TEXT_CONTROLS = "CONTROLS";
+TEXT_TIME = "TIME";
+TEXT_TIME_EXT = "EXT TIME";
+TEXT_VS = "VS";
+TEXT_FINISH = "FINAL WHISTLE";
+TEXT_SELECT_YOUR_TEAM = "SELECT YOUR TEAM";
+TEXT_CREDITS_DEVELOPED = "Developed by";
+TEXT_WIN = "YOU WON";
+TEXT_LOSE = "YOU LOSE";
+TEXT_SCORES = "SCORES";
+TEXT_TIME_EXTENDED = "TIME EXTENDED";
+TEXT_SCORE_GOAL_PLAYER = "SCORE PLAYER GOAL";
+TEXT_SCORE_GOAL_OPPONENT = "SCORE OPPONENT GOAL";
+TEXT_MACTH_SCORE = "SCORE MATCH";
+TEXT_TOTAL_SCORE = "TOTAL SCORE";
+TEXT_CONGRATULATIONS = "CONGRATULATIONS YOU WON ALL MATCHES!";
+TEXT_SCORE_PLAYER_GOAL = "Score player goal";
+TEXT_SCORE_OPPONENT_GOAL = "Score opponent goal";
+TEXT_SCORE_DRAW_MATCH = "Score match draw";
+TEXT_SCOR_WON_MATCH = "Score match won";
+TEXT_MATCH = "MATCH";
+TEXT_KICK_OFF = "KICK OFF";
+TEXT_PAUSE = "PAUSE";
+TEXT_ARE_SURE = "ARE SURE?";
+TEXT_CPU_TURN = "CPU Turn";
+TEXT_PLAYER_TURN = "PLAYER Turn";
+TEXT_FOUL = "FOUL!!";
+TEXT_PT = "PT";
+TEXT_LAUNCH_DELAY_PENALTY = "LAUNCH DELAY PENALTY CPU TURN";
+var TEXT_PRELOADER_CONTINUE = "START";
+
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "Congratulations!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better?";
